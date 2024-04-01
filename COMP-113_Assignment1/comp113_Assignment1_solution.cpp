67b4798249860939933737f7b6b25d57af4e1471
Oleg Rubenchik <oleg.rubenchik25@gmail.com>
2024-04-01 17:01:01 +0300

    Done. Commnets needed

--- COMP-113_Assignment1/comp113_Assignment1_solution.cpp
+++ COMP-113_Assignment1/comp113_Assignment1_solution.cpp
@@ -2,19 +2,44 @@
 * /author Oleg Rubenchik
 */
 #include <iostream>
+#include <string>
 using namespace std;
 int power(int, int); //	A function that raises a number to a certain degree
-void reverseArray(int A[],int revA[], int);
-void displayArray(int A[], int);
 int convertToDecimal(int A[], int);
 bool isBitSet(int num, int pos);
 int main() {
+	char c;
 	const int MAX = 10; // Maximum size of bits
-	int answer[MAX] = { 0,0,1,1 }; // Answer in Binary form
+	int answer[MAX] = {0}; // Answer in Binary form
 	int size; // Actual size of bits
+	string name;
 	int MAX_NUM;
-	int revA[MAX];
-	cout << isBitSet(4,6);
+	cout << "Enter number of bits (<=10): ";
+	cin >> size;
+	cout << "Thank you! Now tell me your name: ";
+	while (c = cin.get(), c != '\n');
+	getline(cin, name);
+	cout << name << ", think of a number between 1 and " << power(2,size) - 1 << " and I will guess it...\n\n";
+	for (int pos = 0; pos < size; pos++) {
+		int countline = 0;
+		for (int i = 1; i <= (power(2, size) - 1); i++) {
+			if (isBitSet(i, pos)) {
+				cout << i << ' ';
+				countline += 1;
+			}
+			if (countline == 10) {
+				countline = 0;
+				cout << '\n';
+			}
+		}
+		cout << '\n';
+		char reply;
+		cout << "Is your number in the above table (y/n): ";
+		cin >> reply;
+		if (reply == 'y') answer[pos] = 1;
+	}
+	int secretNumber = convertToDecimal(answer, size);
+	cout << name << ", you have selected number " << secretNumber << ".";
 	return 0;
 }
 int power(int num,int power) {
@@ -23,15 +48,6 @@ int power(int num,int power) {
 		poweredNum = poweredNum * num;
 	}
 	return poweredNum;
-}
-void reverseArray(int A[],int revA[], int size) {
-	for (int i = 0; i < size; i++)
-		revA[i] = A[size - 1 - i];
-}
-void displayArray(int A[], int size) {
-	for (int i = 0; i < size; i++)
-		cout << A[i] << ' ';
-	cout << '\n';
 }
 int convertToDecimal(int A[], int size) {
 	int num = 0;
@@ -42,17 +58,11 @@ int convertToDecimal(int A[], int size) {
 	}
 	return num;
 }
-bool isBitSet(int num, int pos) {
-	int currentPow = power(2, pos);
-	while (num >= currentPow) {
-		num -= currentPow; // Keep subtracting the power of 2
-		if (num == 0) {
-			return true; // If we hit exactly zero, the bit was set
-		}
-		// Find the next highest power of 2 less than the current number
-		while (currentPow > num) {
-			currentPow /= 2;
-		}
+bool isBitSet(int number, int pos) {
+	if (pos == 0) {
+		return number % 2 == 1; // If pos is 0, just check if number is odd
 	}
-	return false;
+
+	int divisor = power(2, pos);
+	return (number / divisor) % 2 == 1;
 }
